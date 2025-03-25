ba8ff8110bb47fe72370b8598c6eeb3995188a9f
fatema <fatemah.mirzadeh99@gmail.com>
2025-03-25 22:34:29 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import { useEffect, useState } from 'react';
+import './App.css';
+
+const App = () => {
+
+  const [darkMode, setDarkMode] = useState(() => { return JSON.parse(localStorage.getItem("darkMode")) || false })
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark")
+    } else {
+      document.body.classList.remove("dark")
+    }
+  }, [darkMode])
+
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode))
+  }, [darkMode])
+
+
+  return (
+    <div className='toggle-container'>
+
+      <h1>Click here to switch Dark Mode</h1>
+      
+      <label className='switch'>
+        <input
+          type='checkbox'
+          checked={darkMode}
+          onChange={() => setDarkMode(!darkMode)}
+        />
+
+        <span className='slider'></span>
+      </label>
+
+    </div>
+  )
+}
+
+export default App;
